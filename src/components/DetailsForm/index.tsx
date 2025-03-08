dd3e038f34ffbcb483ff67977e87b20cc3cc8e7d
Henrique Garcia <henriquegarcia.dev@gmail.com>
2025-03-08 18:14:29 -0300

    setup

--- src/components/DetailsForm/index.tsx
+++ src/components/DetailsForm/index.tsx
@@ -1,8 +1,11 @@
 // src/components/DetailsForm.tsx
 
+import { useState } from 'react'
+
 import * as S from './styles'
 
-import { Descriptions, DescriptionsProps } from 'antd'
+import { Descriptions, DescriptionsProps, message } from 'antd'
+import { CopyOutlined } from '@ant-design/icons'
 
 import { IUserProfile } from '@/types'
 
@@ -16,15 +19,51 @@ export interface DetailsFormProps extends DescriptionsProps {
 }
 
 const DetailsForm = ({ data, fields, ...rest }: DetailsFormProps) => {
+  const [hoveredKey, setHoveredKey] = useState<string | null>(null)
+
+  const handleCopy = (value: any) => {
+    const textToCopy = value?.toString() || ''
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        message.success('Valor copiado para a área de transferência!')
+      })
+      .catch(() => {
+        message.error('Falha ao copiar o valor.')
+      })
+  }
+
   return (
     <S.StyledDescriptions {...rest}>
-      {fields.map(({ key, label, render }) => (
-        <Descriptions.Item key={key} label={label}>
-          {render
-            ? (render(data[key as keyof IUserProfile]) as React.ReactNode)
-            : (data[key as keyof IUserProfile] as React.ReactNode) || '-'}
-        </Descriptions.Item>
-      ))}
+      {fields.map(({ key, label, render }) => {
+        const value = data[key as keyof IUserProfile]
+        const displayValue = render ? render(value) : value || '-'
+
+        return (
+          <Descriptions.Item key={key} label={label}>
+            <div
+              onMouseEnter={() => value && setHoveredKey(key as string)}
+              onMouseLeave={() => setHoveredKey(null)}
+            >
+              <S.DescriptionContent>
+                {typeof displayValue === 'object'
+                  ? JSON.stringify(displayValue)
+                  : displayValue}
+                {hoveredKey === key && value && (
+                  <S.CopyButton
+                    icon={<CopyOutlined />}
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      handleCopy(value)
+                    }}
+                    size="small"
+                  />
+                )}
+              </S.DescriptionContent>
+            </div>
+          </Descriptions.Item>
+        )
+      })}
     </S.StyledDescriptions>
   )
 }
--- src/components/DetailsForm/styles.ts
+++ src/components/DetailsForm/styles.ts
@@ -1,7 +1,8 @@
 // src/components/UserMenu/styles.ts
 
 import styled from 'styled-components'
-import { Descriptions, theme } from 'antd'
+import { Descriptions, theme, Button } from 'antd'
+
 import Fonts from '@/utils/styles/fonts'
 
 const { useToken } = theme
@@ -9,15 +10,31 @@ const { useToken } = theme
 export const StyledDescriptions = styled(Descriptions)`
   .ant-descriptions-item-label {
     padding: 6px 10px !important;
-
     font-size: ${Fonts.xxxs};
     font-weight: 400;
   }
 
   .ant-descriptions-item-content {
     padding: 8px 12px !important;
-
     font-size: ${Fonts.xxxs};
     font-weight: 400;
+  }
+`
+
+export const DescriptionContent = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`
+
+export const CopyButton = styled(Button)`
+  position: absolute;
+  right: -5px;
+  top: 50%;
+  transform: translateY(-50%);
+
+  &:hover {
+    color: #af6dac;
   }
 `
